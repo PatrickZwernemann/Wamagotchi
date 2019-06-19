a6ac4e4196c092314fc86a9841f61ee3810c5443
Steffen Exler <steffen.exler@gmail.com>
2019-06-19 11:33:52 +0200

    init spring boot

---
+++ src/main/java/com/wamagotchi/wamagotchi/WamagotchiApplication.java
@@ -0,0 +1,13 @@
+package com.wamagotchi.wamagotchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WamagotchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WamagotchiApplication.class, args);
+	}
+
+}
